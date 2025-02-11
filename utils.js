9fdd9f1fe6c1f077b0f451cb8d408960d6ee6c87
Rishi Jha <rishijha2312@gmail.com>
2025-02-11 18:27:01 +0530

    fixed folder structure and added openAI for data extraction

--- utils.js
+++
@@ -1,6 +0,0 @@
-export const sanitizeInput = (input) => {
-    if (typeof input !== 'string') return '';
-    return input
-        .replace(/[<>]/g, '')  // Remove potential HTML tags
-        .trim();
-};
