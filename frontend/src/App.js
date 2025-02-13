efe2659c3c8733ad2646bb69ba534828b93aac23
Rishi Jha <rishijha2312@gmail.com>
2025-02-14 00:33:24 +0530

    Revert "Added auth"
    This reverts commit 6186a3a2da5a47f8cba454990524a40ccd7f4417.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
 import ResumeUploadPage from './components/ResumeUploadPage';
 import PortfolioPage from './components/PortfolioPage';
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <Routes>
-          {/* Auth Routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-
-          {/* Protected Routes */}
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <ResumeUploadPage />
-              </PrivateRoute>
-            }
-          />
-
-          <Route path="/portfolio/:resumeId" element={<PortfolioPage />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<ResumeUploadPage />} />
+        <Route path="/portfolio/:resumeId" element={<PortfolioPage />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
