089618895ca54a0fd834672daf007de8e5e7274c
Rishi Jha <rishijha2312@gmail.com>
2025-02-14 00:30:39 +0530

    Added auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 import ResumeUploadPage from './components/ResumeUploadPage';
 import PortfolioPage from './components/PortfolioPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ResumeUploadPage />} />
-        <Route path="/portfolio/:resumeId" element={<PortfolioPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <ResumeUploadPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route path="/portfolio/:resumeId" element={<PortfolioPage />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
 
 export default App;
-
